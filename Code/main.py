d0363c9efe3591ef12fa09cfebe5b31d6146338b
ianjali <amudgal26@gwmail.gwu.edu>
2024-08-19 22:02:13 -0400

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -3,11 +3,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
-from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import make_pipeline
+# from imblearn.over_sampling import SMOTE
+#from imblearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.layers import LSTM, Dense, Embedding, Dropout,Input, Attention, Layer, Concatenate, Permute, Dot, Multiply, Flatten
+from keras.layers import RepeatVector, Dense, Activation, Lambda
+from keras.models import Sequential
+from keras import backend as K, regularizers, Model, metrics
+from keras.backend import cast
 
 CUR_DIR = os.getcwd()
 PARENT_DIR = os.path.dirname(CUR_DIR)
@@ -89,16 +99,35 @@ X_test = Standard_Scaler(X_test, col_names)
 # We are going to ensure that we have the same splits of the data every time.
 # We can ensure this by creating a KFold object, kf, and passing cv=kf instead of the more common cv=5.
 
-kf = StratifiedKFold(n_splits=5, shuffle=False)
-rf = RandomForestClassifier(n_estimators=100, random_state=13)
+# kf = StratifiedKFold(n_splits=5, shuffle=False)
+# rf = RandomForestClassifier(n_estimators=100, random_state=13)
+#
+# score = cross_val_score(rf, X_train, Y_train, cv=kf, scoring='recall')
+# print("Cross Validation Recall scores are: {}".format(score))
+# print("Average Cross Validation Recall score: {}".format(score.mean()))
+#
+# smote_pipeline = make_pipeline(SMOTE(random_state=42),
+#                               RandomForestClassifier(n_estimators=100, random_state=13))
+#
+# score3 = cross_val_score(smote_pipeline, X_train, Y_train, scoring='recall', cv=kf)
+# print("Cross Validation Recall Scores are: {}".format(score3))
+# print("Average Cross Validation Recall score: {}".format(score3.mean()))
 
-score = cross_val_score(rf, X_train, Y_train, cv=kf, scoring='recall')
-print("Cross Validation Recall scores are: {}".format(score))
-print("Average Cross Validation Recall score: {}".format(score.mean()))
+### Reshape input to be 3D [samples, timesteps, features] (format requis par LSTM)
+#X_train =  X_train.values.reshape(new_shape)
+train_LSTM_X = X_train.values.reshape((X_train.shape[0], 1, X_train.shape[1]))
+test_LSTM_X = X_test.values.reshape((X_test.shape[0], 1, X_test.shape[1]))
 
-smote_pipeline = make_pipeline(SMOTE(random_state=42),
-                              RandomForestClassifier(n_estimators=100, random_state=13))
+inputs=Input((1,11))
+x1=LSTM(50,dropout=0.3,recurrent_dropout=0.2, return_sequences=True)(inputs)
+x2=LSTM(50,dropout=0.3,recurrent_dropout=0.2)(x1)
+outputs=Dense(1,activation='sigmoid')(x2)
+model=Model(inputs,outputs)
+import tensorflow
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=[tensorflow.keras.metrics.Recall()])
+history=model.fit(train_LSTM_X, Y_train,epochs=200,batch_size=20000)
 
-score3 = cross_val_score(smote_pipeline, X_train, Y_train, scoring='recall', cv=kf)
-print("Cross Validation Recall Scores are: {}".format(score3))
-print("Average Cross Validation Recall score: {}".format(score3.mean()))
+#isolation forest
+clf = IsolationForest(contamination=0.1, random_state=42)
+clf.fit(X_train)
+
